385edf5808029e6b4d7043345f4d7db130fa7363
Paul Reioux <reioux@gmail.com>
2013-04-14 00:50:10 -0500

    dynamic filesync: add some cache optimizations
    and clean up file format and typos
    
    bump version to 1.1
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -170,7 +170,7 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else {
 #endif
@@ -213,7 +213,7 @@ static int do_fsync(unsigned int fd, int datasync)
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif
@@ -223,7 +223,7 @@ SYSCALL_DEFINE1(fsync, unsigned int, fd)
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif
@@ -298,7 +298,7 @@ SYSCALL_DEFINE(sync_file_range)(int fd, loff_t offset, loff_t nbytes,
 				unsigned int flags)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else {
 #endif
@@ -402,7 +402,7 @@ SYSCALL_DEFINE(sync_file_range2)(int fd, unsigned int flags,
 				 loff_t offset, loff_t nbytes)
 {
 #ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && !early_suspend_active)
+	if (unlikely(dyn_fsync_active && !early_suspend_active))
 		return 0;
 	else
 #endif
