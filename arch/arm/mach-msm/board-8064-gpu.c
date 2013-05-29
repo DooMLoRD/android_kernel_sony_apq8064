d2673c428624356dc57f961d07a891769fdb2fad
DooMLoRD <metoo.mememe@gmail.com>
2013-05-29 19:53:18 +0530

    added GPU overclock to 533 MHz

--- arch/arm/mach-msm/board-8064-gpu.c
+++ arch/arm/mach-msm/board-8064-gpu.c
@@ -229,14 +229,24 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 #ifdef CONFIG_GPU_OVERCLOCK
 		{
+			.gpu_freq = 533333000,
+			.bus_freq = 6,
+			.io_fraction = 0,
+		},
+		{
 			.gpu_freq = 487500000,
-			.bus_freq = 4,
+			.bus_freq = 5,
 			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 400000000,
-			.bus_freq = 3,
+			.bus_freq = 4,
 			.io_fraction = 0,
+		},
+		{
+			.gpu_freq = 325000000,
+			.bus_freq = 3,
+			.io_fraction = 33,
 		},
 #else
 		{
