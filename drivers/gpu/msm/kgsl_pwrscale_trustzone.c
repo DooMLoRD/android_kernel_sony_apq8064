e3e0c4a749989278c8e79178eb2a32df2ee56d2c
DooMLoRD <metoo.mememe@gmail.com>
2013-08-05 02:51:18 +0530

    [PATCH] KGSL: Simple Governor: fix logic

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -240,17 +240,22 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 		idle = priv->bin.total_time - priv->bin.busy_time;
 		idle = (idle > 0) ? idle : 0;
 #ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-	if (priv->governor == TZ_GOVERNOR_SIMPLE) {
-		val = simple_governor(device, idle);
-	}
-#endif
+		if (priv->governor == TZ_GOVERNOR_SIMPLE)
+			val = simple_governor(device, idle);
+		else
+			val = __secure_tz_entry(TZ_UPDATE_ID, idle, device->id);
+#else
 		val = __secure_tz_entry(TZ_UPDATE_ID, idle, device->id);
+#endif
 	}
 	priv->bin.total_time = 0;
 	priv->bin.busy_time = 0;
-	if (val)
+	if (val) {
 		kgsl_pwrctrl_pwrlevel_change(device,
 					     pwr->active_pwrlevel + val);
+		//pr_info("TZ idle stat: %d, TZ PL: %d, TZ out: %d\n",
+		//		idle, pwr->active_pwrlevel, val);
+	}
 }
 
 static void tz_busy(struct kgsl_device *device,
