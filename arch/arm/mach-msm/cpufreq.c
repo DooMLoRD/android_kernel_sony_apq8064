3241f58cde2aa8a0b8b51aeb3b438742f60ec171
DooMLoRD <metoo.mememe@gmail.com>
2013-09-05 02:32:58 +0530

    added intellidemand CPU freq governor (updated to v4.2) thanks to faux123

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -299,6 +299,45 @@ static struct notifier_block __refdata msm_cpufreq_cpu_notifier = {
 	.notifier_call = msm_cpufreq_cpu_callback,
 };
 
+#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
+extern bool lmf_screen_state;
+#endif
+
+static void msm_cpu_early_suspend(struct early_suspend *h)
+{
+#ifdef CONFIG_CPUFREQ_LIMIT_MAX_FREQ
+	int cpu = 0;
+
+	for_each_possible_cpu(cpu) {
+		mutex_lock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
+lmf_screen_state = false;
+		mutex_unlock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
+	}
+#endif
+
+}
+
+static void msm_cpu_late_resume(struct early_suspend *h)
+{
+#ifdef CONFIG_CPUFREQ_LIMIT_MAX_FREQ
+	int cpu = 0;
+
+	for_each_possible_cpu(cpu) {
+
+		mutex_lock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
+lmf_screen_state = true;
+		mutex_unlock(&per_cpu(cpufreq_suspend, cpu).suspend_mutex);
+	}
+#endif
+
+}
+
+static struct early_suspend msm_cpu_early_suspend_handler = {
+.level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN,
+.suspend = msm_cpu_early_suspend,
+.resume = msm_cpu_late_resume,
+};
+
 static int msm_cpufreq_suspend(void)
 {
 	int cpu;
@@ -368,7 +407,7 @@ static int __init msm_cpufreq_register(void)
 	}
 
 	register_hotcpu_notifier(&msm_cpufreq_cpu_notifier);
-
+	register_early_suspend(&msm_cpu_early_suspend_handler);
 	register_pm_notifier(&msm_cpufreq_pm_notifier);
 	return cpufreq_register_driver(&msm_cpufreq_driver);
 }
